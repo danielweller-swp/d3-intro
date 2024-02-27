40f281bcf3240a562ee5177dfd9ac736ae08816b
Felix Krause <felix_krause1999@yahoo.de>
2024-02-27 22:18:04 +0100

    [WIP] updates scatter plot and style

--- best-practice-example/static/scatter-plot.js
+++ best-practice-example/static/scatter-plot.js
@@ -1,4 +1,5 @@
-// Load data from CSV
+// Description: This file contains the code to display a scatter plot in the right column of the web page.
+
 function displayScatterPlot() {
     console.log("Displaying scatter plot")
 
@@ -6,17 +7,20 @@ function displayScatterPlot() {
     d3.selectAll("#scatterPlot").remove();
 
     // Load data from CSV and build scatter plot
-    d3.text("static/data/scatter_data.csv").then(data => {
+    d3.text("static/data/data.csv").then(data => {
         // Parse data to array of numbers
-        var parsedData = d3.csvParseRows(data, d => [+d[0], +d[1]]);
+        var parsedData = d3.csvParseRows(data, d => [+d[1], +d[2]]);
+
+        // Drop the first row (column names)
+        parsedData = parsedData.slice(1);
 
         // Store values for svg creation
-        const parent = d3.select("#right_column")
+        var parent = d3.select("#right_column")
         const width = parent._groups[0][0]["clientWidth"];
-        const height = width * 0.6;
+        const height = width * 0.6 + 20;  // make plot height always 60% of the width
 
-        // Define padding
-        var padding = {top: 0, right: 10, bottom: 20, left: 30};
+        // Define padding around the svg
+        var padding = {top: 0, right: 10, bottom: 50, left: 50};
 
         // Append SVG
         var svg = parent.append("svg")
@@ -24,8 +28,7 @@ function displayScatterPlot() {
             .attr("width", width)
             .attr("height", height);
 
-
-        // X and Y scales
+        // x and y scales
         const xMinMax = d3.extent(parsedData, d => d[0]) // returns list [min, max] of provided data
         var xScale = d3.scaleLinear()
             .domain([xMinMax[0] * 0.95, xMinMax[1] * 1.05])
@@ -36,27 +39,15 @@ function displayScatterPlot() {
             .domain([yMinMax[0] * 0.95, yMinMax[1] * 1.05])
             .range([height - padding.bottom, padding.top]);  // Note that the "origin" is defined as the upper left corner, thus we need to invert the range
 
-        // X axis
+        // Plot x axis
         svg.append("g")
             .attr("transform", "translate(0," + (height - padding.bottom) + ")")
             .call(d3.axisBottom(xScale))
-            .append("text")
-            .attr("class", "label")
-            .attr("x", width)
-            .attr("y", -6)
-            .style("text-anchor", "end")
-            .text("Coffee Consumption");
 
-        // Y axis
+        // Plot y axis
         svg.append("g")
             .attr("transform", "translate(" + (padding.left) + ",0)")
             .call(d3.axisLeft(yScale))
-            .append("text")
-            .attr("class", "label")
-            .attr("y", 6)
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .text("Flamingo Enthusiasm");
 
         // Create circles for each data point
         svg.selectAll(".dot")
@@ -66,5 +57,22 @@ function displayScatterPlot() {
             .attr("cx", d => xScale(d[0]))
             .attr("cy", d => yScale(d[1]))
             .attr("r", 2);
+
+        // Add an x-axis label
+        svg.append("text")
+            .attr("class", "label")
+            .attr("x", padding.left + (width - padding.left - padding.right) / 2)
+            .attr("y", height - 6)
+            .style("text-anchor", "middle")
+            .text("Coffee Consumption");
+
+        // Add a y-axis label
+        svg.append("text")
+            .attr("class", "label")
+            .attr("transform", "rotate(-90)")
+            .attr("x", -(height - padding.top - padding.bottom)/2  - padding.top)
+            .attr("dy", ".72em")
+            .style("text-anchor", "middle")
+            .text("Flamingo Enthusiasm Level");
     });
 }
